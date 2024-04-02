1ea0252229d15bb84a015f9935c938f1e0b810d9
Your Namechase <you@example.comcefeingold10@icloud.com>
2024-04-02 11:13:04 -0400

    Add db file

---
+++ Database/bsp.sql
@@ -0,0 +1,7 @@
+CREATE TABLE bsp(Rank INTEGER PRIMARY KEY, Basketball TEXT);
+
+INSERT INTO bsp VALUES (1, "Michael Jordan");
+INSERT INTO bsp VALUES (2, "Lebron James");
+INSERT INTO bsp VALUES (3, "Kobe Bryant");
+INSERT INTO bsp VALUES (4, "Wilt Chamberlin");
+INSERT INTO bsp VALUES (5, "Steph Curry");
