efbda584ebb207626f6ae4e9b1f4d650225d0867
Your Namechase <you@example.comcefeingold10@icloud.com>
2024-04-11 10:13:29 -0400

    change template to .tpl

--- Mybottle/run.py
+++ Mybottle/run.py
@@ -10,4 +10,5 @@ def message(name):
     the_message = f'Hello {name}!'
     return bottle.template('message_template', message=the_message)
 
-bottle.run(host='0.0.0.0', port=8090)
+bottle.run(host='0.0.0.0',port=8090)
+
