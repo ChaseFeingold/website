00a23e8e3c2a19a20ed0ec9806ab525884c4d248
Your Namechase <you@example.comcefeingold10@icloud.com>
2024-04-11 10:47:30 -0400

    change back run.py

--- Mybottle/run.py
+++ Mybottle/run.py
@@ -3,12 +3,11 @@ import bottle
 
 @bottle.route('/')
 def index():
-    return bottle.template('index')
+     return bottle.template('index')
 
 @bottle.route('/<name>')
 def message(name):
     the_message = f'Hello {name}!'
     return bottle.template('message_template', message=the_message)
 
-bottle.run(host='0.0.0.0',port=8090)
-
+bottle.run(host='0.0.0.0', port=8090)
